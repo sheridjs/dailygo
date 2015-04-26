1e092755a864d6a0a11cc697ed0ee79d9915db50
Jay Sheridan <sheridjs@gmail.com>
2015-04-26 10:06:33 -0500

    Initial name game.

---
+++ c211/namegame/name-game.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// DailyProgrammer challenge #211 (Easy) from 4/20/2015
+// - http://www.reddit.com/r/dailyprogrammer/comments/338p28/20150420_challenge_211_easy_the_name_game/
+func main() {
+	fmt.Println("It's the name game!")
+}
+
+////// References
+// http://en.wikipedia.org/wiki/The_Name_Game#Rules
+// https://blog.golang.org/strings
+// http://golang.org/pkg/strings/
+// http://golang.org/pkg/unicode/
+// http://golang.org/pkg/unicode/utf8/
